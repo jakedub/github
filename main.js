117f3944a1f3fb073495b366ce71e167d29d3bac
jakedub <jacob.w.moore@gmail.com>
2017-08-08 09:48:56 -0400

    updated for bio

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ console.log(req);
 
 function reqListener () {
   let data = JSON.parse(this.responseText);
-  let name = `${data.name}`;
+  let name = `${data.name}`; //(data.name)
   let hub = `GitHub URL: ${data.url}`;
   let email = `Email: ${data.email}`;
   let company = `Company: ${data.company}`;
@@ -20,11 +20,25 @@ function reqListener () {
   let image= `Image ${data.avatar_url}`;
   }
   // callBack(bio);
-  console.log(reqListener);
+let li1=document.createElement("li");
+let li2=document.createElement("li");
+let li3=document.createElement("li");
+let li4=document.createElement("li");
+let li5=document.createElement("li");
+li1.textContent = name; //do this for hub, email, company, website
+li2.textContent = hub;
+li3.textContent = email;
+li4.textContent = company;
+li5.textContent = website;
+basics.appendChild(li1);
+basics.appendChild(li2);
+basics.appendChild(li3);
+basics.appendChild(li4);
+basics.appendChild(li5);
 
 
-// function demo(name, hub, email, company, website){
-// }
+function demo(name, hub, email, company, website){
+}
 //
 // function biography(){
 //   story.appendChild()//assuming a callback from reqListener here
