9efd1253c1f2022aafb1a3ebd170735c106a1733
jakedub <jacob.w.moore@gmail.com>
2017-08-09 16:47:21 -0400

    needs work on the CSS

--- main.js
+++ main.js
@@ -8,25 +8,15 @@ function reqListener () {
   let data = JSON.parse(this.responseText);
     let name = (data.name);
     let hub = (data.url);
-    let email = (data.blog);
+    let email = (data.email);
     let company = (data.company);
     let picture = (data.avatar_url);
-    // let bio = (data.bio);
-    let li1 = document.createElement("li");
-    let li2 = document.createElement("li");
-    let li3 = document.createElement("li");
-    let li4 = document.createElement("li");
-    // let p1 = document.createElement("p");
-    li1.textContent = name;
-    li2.textContent = hub;
-    li3.textContent = email;
-    li4.textContent = company;
-    // p1.textContent = p1;
-    basics.appendChild(li1);
-    basics.appendChild(li2);
-    basics.appendChild(li3);
-    basics.appendChild(li4);
-    contactInfo.innerHTML = ``;
+    contactInfo.innerHTML = `
+    <li>Name: ${data.name}</li>
+    <li>Website: ${data.url}</li>
+    <li>Email: ${data.email}</li>
+    <li>Company: ${data.company}</li>
+    `;
     storyInfo.innerHTML = `<p>${data.bio}</p><img src="${picture}">`;
     header.append(name);
     basics.appendChild(contactInfo);
